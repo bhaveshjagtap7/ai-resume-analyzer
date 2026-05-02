4f519c8bd8b023701054c6717bd1c975083212ea
bhavesh <bhaveshjagtap38@gmail.com>
2026-05-02 23:39:15 +0530

    initial commit

---
+++ backend/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import os
+from resume_parser import extract_text
+from skill_extractor import extract_skills
+
+app = Flask(__name__)
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+@app.route("/")
+def home():
+    return "AI Resume Analyzer Running"
+
+@app.route("/upload", methods=["POST"])
+def upload_resume():
+    file = request.files["resume"]
+    filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+    file.save(filepath)
+
+    text = extract_text(filepath)
+    skills = extract_skills(text)
+
+    return jsonify({
+        "skills": skills,
+        "message": "Resume analyzed successfully"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
